7186eacfee177321c0ee5ee97d53bfd84134e61d
CpHarding <CpHarding@users.noreply.github.com>
2018-03-04 22:04:10 +0000

    tidy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyconfig',
+    version='1.0.0',
+    packages=['pyconfig'],
+    url='https://github.com/CpHarding/pyconfig',
+    license='GNU General Public License v3.0',
+    author='CpHarding',
+    author_email='',
+    description=''
+)
